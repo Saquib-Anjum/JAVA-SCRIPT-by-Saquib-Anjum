4695751befead89b1fca3d16b36d80a5a822aeca
Saquib Anjum <saquibanjum8969@gmail.com>
2024-08-10 01:14:48 +0530

    DOM

---
+++ 11_DOM/card_project/script.js
@@ -0,0 +1,9 @@
+var main = document.getElementById("main");
+var s = "";
+for(let i=0;i<=59;i++){
+    let arr = ["image1.jpeg","image2.jpeg","image3.jpeg","image4.jpeg","image5.jpeg","image5.jpeg"];
+    let a =Math.floor(Math.random()*5)
+    s+=`<div class="card"><img src="${arr[a]}"></div>`;
+}
+main.innerHTML=s;
+alert("ruk ja bhai ")
